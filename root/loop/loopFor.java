fab9614e7f3ac55c327318e72792814d764daf9b
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-02 17:20:19 +0200

    Boucles for

---
+++ root/loop/loopFor.java
@@ -0,0 +1,16 @@
+package root.loop;
+
+public class loopFor {
+    public static void main(String[] args) {
+        
+        //TODO: Créez une boucle "for" qui affiche la phrase "I am having so much fun!" 5 fois
+
+      for ( int i=0; i <5; i++) {
+      // déclarations exécutées à chaque itération
+
+      System.out.println("Valeur de i : " + i + ".");
+      System.out.println("I am having so much fun!");
+      }
+        
+    }
+}
