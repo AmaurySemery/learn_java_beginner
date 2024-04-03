bd361eac9badb2f8ef6c4a103b252460d364e7ee
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-03 19:02:28 +0200

    De même pour scopeVariable en corrigeant les erreurs

--- root/scopeVariable/PublicClass.java
+++ root/scopeVariable/PublicClass.java
@@ -3,14 +3,14 @@ package root.scopeVariable;
 public class PublicClass {
     public boolean publicProperty = true;
     int internalProperty = 0; //par défaut pour package-private
-    private String fileprivateProperty = "Hello!"
+    private String fileprivateProperty = "Hello!";
     private static void privateMethod() {
     }
 }
 
 class PrivateClass {
     int internalProperty = 0; // assigne automatiquement package-private par défaut
-    protected defaultProperty = true; // assigne automatiquement package-private
+    //protected defaultProperty = true; // assigne automatiquement package-private
     public boolean publicProperty = true; // convertit automatiquement en package-private
     private String fileprivateProperty = "Hello!"; //disponible seulement pour la classe
     private static void privateMethod() {
