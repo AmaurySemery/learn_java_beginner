ad310c4596055741f4ab0acf2da496b8304f751d
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-04 18:34:29 +0200

    Mise à jour

--- root/polymorphisme/FigureGeometrique.java
+++ root/polymorphisme/FigureGeometrique.java
@@ -2,25 +2,24 @@ package root.polymorphisme;
 
 // Classe mère spécialisée en Carre avec le mot clé "extends"
 // => La classe Carre étend la classe FigureGeomtrique
-public class FigureGeometrique {
+class FigureGeometrique {
     private int x;
     private int y;
-    public void moveTo(int newX, int newY) {
-        this.x = newX;
-        this.y = newY;
+    FigureGeometrique(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
 
 // La classe fille Carre récupère automatiquement tous les attributs de la classe mère
 // Lorsqu'on fait de l'héritage, tous les champs sont hérités => ils peuvent donc être manipulés si leur accessibilité le permet
-public class Carre extends FigureGeometrique {
-    private long cote;
-    public long getCote() {
-        return cote;
+class Carre extends FigureGeometrique {
+    long cote;
+     Carre(long cote, int x, int y){
+       //Appel du constructeur de la classe mère FigureGeometrique
+       super(x, y);
+       this.cote = cote;
     }
-    public long getPerimetre(){
-        return 4*cote;
-    }
-}
+  }
 
 // En Java, une classe fille ne peut hériter que d'une seule classe mère, mais rien n'empêche cette classe mère d'être la classe fille d'une autre classe mère
