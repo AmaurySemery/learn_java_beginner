b86545fd3163f2fcf0cc376645020128a744c7c2
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-06-20 16:12:17 +0200

    Listes modifiables

--- root/table/OC.java
+++ root/table/OC.java
@@ -1,5 +1,8 @@
 package root.table;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OC { 
     public static void main(String[] args) {
         String[] tableau = new String[10];
@@ -13,6 +16,15 @@ public class OC {
         for(int i = 0; i < tableau.length; i++) {
             System.out.println(tableau[i]);
         }
-    
+
+        List<Integer> myList = new ArrayList<Integer>(); // -> []
+        myList.add(7); // -> [7]
+        myList.add(5); //-> [7, 5]
+        myList.add(1,12); //-> [7, 12, 5]
+        myList.set(0,4); // -> [4, 12, 5]
+        myList.remove(1); // removed 12 -> [4, 5]
+        System.out.println(myList);
+        System.out.println(myList.size());
+        
 }
 }
