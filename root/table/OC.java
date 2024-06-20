3fbf6525d2b6e1a31e6c1d731353243d3d79dd9a
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-06-20 16:05:38 +0200

    Table

---
+++ root/table/OC.java
@@ -0,0 +1,18 @@
+package root.table;
+
+public class OC { 
+    public static void main(String[] args) {
+        String[] tableau = new String[10];
+
+        tableau[3] = "Index 3";
+
+        tableau[1] = "Index 1";
+
+        tableau[3] = "Nouvelle donnée";
+
+        for(int i = 0; i < tableau.length; i++) {
+            System.out.println(tableau[i]);
+        }
+    
+}
+}
